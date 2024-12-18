544df2e461837a016c35f3c481e6e65cda50efc8
J-Ion <90089909+J-Ion@users.noreply.github.com>
2024-12-18 13:58:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true, 
-  };
   
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // URL 인코딩된 데이터 처리
 app.use(cookieParser());
