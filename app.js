948b305f82848770c4da6119163c1b28ef44e1f8
J-Ion <90089909+J-Ion@users.noreply.github.com>
2024-12-19 11:07:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,64 @@ app.use(
     })
 );
 
+app.get('/', (req, res) => {
+    const nonce = res.locals.nonce; // nonce 값을 가져옴
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="ko">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="Content-Security-Policy" content="style-src 'self' 'unsafe-inline' *; script-src 'self' 'nonce-${nonce}' https://unpkg.com;">
+          <link rel="stylesheet" href="/style.css?after" />
+          <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+          <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+          <link rel="stylesheet" as="style" crossorigin
+              href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css" />
+          <link rel="stylesheet" as="style" crossorigin
+              href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard.css" />
+          <title>Ticket!</title>
+      </head>
+      <body>
+          <div class="WelcomeContainer">
+              <h1 class="Welcome" style="z-index:2" data-aos="fade-up">Meet Teddy</h1>
+          </div>
+  
+          <div class="IntroduceContainerUp" data-aos="fade-up">
+              <div class="IntroduceContainer">
+                  <div class="IntroducePicUp">
+                      <img class="IntroducePic" src="/public/images/1st_introduce_pic.png" alt="Cannot show the pictures" />
+                  </div>
+                  <div class="InviteContainer">
+                      <h1 class="Invite" data-aos="fade-up">We<br>Invite You<br>to Our Concert</h1>
+                  </div>
+              </div>
+          </div>
+          <div class="Ultimate" data-aos="fade-up">
+              <div class="ReadyContainerUp">
+                  <div class="ReadyContainer">
+                      <h1 class="Ready" data-aos="fade-up">Ready<br>to<br>Take off?</h1>
+                      <div class="btn-hover color-9Up">
+                          <a href="login.html"><button data-aos="fade-up" class="btn-hover color-9">Get Started</button></a>
+                      </div>
+                  </div>
+                  <div>
+                      <div class="ReadyPicUp" data-aos="fade-up">
+                          <img class="ReadyPic" src="/public/images/flight.jpeg" alt="Cannot show the pictures off" />
+                      </div>
+                  </div>
+              </div>
+          </div>
+  
+          <script nonce="${nonce}">
+              AOS.init();
+          </script>
+      </body>
+      </html>
+    `);
+  });
+  
+
 // 속도 제한 설정 (IP당 1분에 100개의 요청 허용)
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1분
