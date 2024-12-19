5c79a8390f9e6922f18d3987b397d68c210377a4
J-Ion <90089909+J-Ion@users.noreply.github.com>
2024-12-19 10:19:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
     helmet({
         contentSecurityPolicy: {
             directives: {
-                defaultSrc: ["'self'"],
+                defaultSrc: ["'self'", "*"],
                 scriptSrc: ["'self'", 'https://unpkg.com'], // 외부 스크립트 허용
                 styleSrc: ["'self'", "'unsafe-inline'", 'https://unpkg.com'], // 외부 스타일 허용
             },
